60384dd8dddb0d07a8e0386602bb87cd784ca4da
namrajp <active99raj@gmail.com>
2025-11-24 21:40:36 +1100

    initial commit

---
+++ first-single-file-component/src/App-text-interpolation.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from "vue";
+import { reactive } from "vue";
+// component logic
+// declare some reactive state here.
+const message = ref("Hello World!");
+console.log(message);
+message.value = "new value";
+
+const counter = reactive({
+  count: 0,
+});
+counter.count++;
+console.log(counter.count);
+</script>
+
+<template>
+  <h1>{{ message.split(" ").reverse().join("") }}</h1>
+  <p>Count is: {{ counter.count }}</p>
+</template>
+
+<style scoped></style>
---
+++ first-single-file-component/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+
+// const titleClass = ref("title");
+const titleId = ref("title");
+</script>
+
+<template>
+  <h1 :id="titleId">Make me red</h1>
+  <!-- add dynamic class binding here -->
+</template>
+
+<style>
+#title {
+  color: red;
+}
+</style>
